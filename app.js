a820eec8f281ea3b97e0f8075967d3e63c726f2b
deleot <deleo.t@northeastern.edu>
2023-08-08 18:58:12 -0400

    a5

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-import express from "express-session";
-import cors from 'cors'
+import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+import session from "express-session";
+
 const app = express();
 app.use(
- cors({
-   credentials: true,
-   origin: "http://localhost:3000",
- })
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
 );
 const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
-  };
-  app.use(
-    session(sessionOptions)
-  );
-  
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
-const port = process.env.PORT || 4000;
-app.use(express.json());
-HelloController(app);
-TuitsController(app);
-UserController(app);
-app.use(cors())
-app.listen(4000);
 AuthController(app);
+TuitsController(app);
+HelloController(app);
+UserController(app);
+
+app.listen(process.env.PORT || 4000);
